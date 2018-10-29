44d77792fab76a83806bc1275333e814f53b3a29
Bruno Martins <bgcngm@gmail.com>
2018-10-29 12:37:02 +0000

    camera: Use generated kernel headers
    Change-Id: I1508ce54fd99e87f2857e6c6141d5e90f5e9892b

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -1,7 +1,6 @@
 OLD_LOCAL_PATH := $(LOCAL_PATH)
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/../../../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_HEADER_LIBRARIES := libutils_headers
@@ -12,8 +11,7 @@ LOCAL_CFLAGS+= -D_ANDROID_ -DQCAMERA_REDEFINE_LOG
 
 LOCAL_CFLAGS += -Wall -Wextra -Werror -Wno-unused-parameter
 
-LOCAL_C_INCLUDES+= $(kernel_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 
 LIB2D_ROTATION=false
 
@@ -71,7 +69,6 @@ LOCAL_SRC_FILES := \
     src/mm_jpeg_mpo_composer.c
 
 LOCAL_MODULE           := libmmjpeg_interface
-LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libqomx_core libmmcamera_interface
 ifeq ($(strip $(LIB2D_ROTATION)),true)
     LOCAL_SHARED_LIBRARIES += libmmlib2d_interface
