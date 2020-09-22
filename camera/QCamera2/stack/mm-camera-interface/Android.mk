a8257323dc947ef267c1ca6e81794f3e9fc6d75b
Bruno Martins <bgcngm@gmail.com>
2020-09-22 14:47:33 +0000

    camera: Drop usage of LOCAL_COPY_HEADERS
    Google deprecated LOCAL_COPY_HEADERS in Android 11,
    so use LOCAL_EXPORT_C_INCLUDE_DIRS instead.
    
    Ref: https://android.googlesource.com/platform/build/soong/+/master/docs/best_practices.md#headers.
    
    Change-Id: Id68bc83cf74f2af0037331ebe4d17819021b88e8

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -5,6 +5,8 @@ include $(LOCAL_PATH)/../../../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_HEADER_LIBRARIES := libhardware_headers
+LOCAL_HEADER_LIBRARIES += camera_common_headers
+LOCAL_HEADER_LIBRARIES += camera_fdleak_headers
 LOCAL_HEADER_LIBRARIES += media_plugin_headers
 
 MM_CAM_FILES := \
@@ -36,9 +38,6 @@ ifneq (,$(filter msm8996 msmcobalt msmfalcon,$(TARGET_BOARD_PLATFORM)))
 endif
 
 LOCAL_CFLAGS += -D_ANDROID_ -DQCAMERA_REDEFINE_LOG
-LOCAL_COPY_HEADERS_TO := mm-camera-interface
-LOCAL_COPY_HEADERS += ../common/cam_intf.h
-LOCAL_COPY_HEADERS += ../common/cam_types.h
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
