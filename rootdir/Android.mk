02989443475194c3300da13cb323b2fcf8a85ca4
Quentin BOUTEILLER <quentin@quent1.fr>
2022-05-12 19:57:26 +0200

    Fix init.device.rc name

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,10 +11,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.j8y18lte.rc
+LOCAL_MODULE       := init.gta2xlwifi.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.j8y18lte.rc
+LOCAL_SRC_FILES    := init.gta2xlwifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
