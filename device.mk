997c6baeaab3d3e1e2bd57a065928d2e6bb026e0
UDPSendToFailed <udpsendtofailed@gmail.com>
2022-05-19 21:23:09 +0200

    Clearkey DRM fix

--- device.mk
+++ device.mk
@@ -163,10 +163,11 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    libprotobuf-cpp-lite
 
 # FastCharge
 PRODUCT_PACKAGES += \
