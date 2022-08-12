b23b4bc595c4a184fd673fc59fce66aeff6c92fa
UDPSendToFailed <udpsendtofailed@gmail.com>
2022-08-12 15:33:04 +0200

    Add init.qcom.post_boot.sh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -83,3 +83,10 @@ LOCAL_SRC_FILES    := ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.post_boot.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.qcom.post_boot.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
