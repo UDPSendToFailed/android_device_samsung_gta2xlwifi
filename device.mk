ec22aecfc8b03afd63ff4484126df8d32b9571de
UDPSendToFailed <udpsendtofailed@gmail.com>
2024-06-13 15:45:06 +0200

    No need to hardcode this, it can be enabled in developer options

--- device.mk
+++ device.mk
@@ -367,17 +367,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dsi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/dsi_config.xml \
     $(LOCAL_PATH)/configs/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml
 
-# For userdebug builds
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.secure=0 \
-    ro.adb.secure=0 \
-    ro.debuggable=1 \
-    persist.sys.root_access=1 \
-    persist.service.adb.enable=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb
-    
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/gta2xlwifi/gta2xlwifi-vendor.mk)
 
