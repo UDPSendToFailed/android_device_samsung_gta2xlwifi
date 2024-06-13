232be6d92274b71642f92ce1199f905280c90407
UDPSendToFailed <udpsendtofailed@gmail.com>
2024-06-13 15:34:37 +0200

    Move Bluetooth audio packages under Bluetooth category

--- device.mk
+++ device.mk
@@ -84,8 +84,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.bluetooth.default \
     audio.primary.msm8953 \
@@ -119,6 +117,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0 \
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.0-service \
     android.hardware.bluetooth@1.0.vendor \
     libbt-vendor
