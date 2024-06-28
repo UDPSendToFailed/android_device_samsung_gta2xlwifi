098215f1a5f8c97cbc47d30dad6085b107fd861a
someone5678 <someone5678_github@proton.me>
2024-06-29 01:02:42 +0900

    libqti-perfd-client: Correct report values and types of methods
    * Correct report values and types of methods
    * Also add more dummy methods
    
    * Addded:
    perf_event
    perf_get_feedback_extn
    perf_wait_get_prop
    
    * Required for some blobs.
    
    e.g.) vendor/bin/qsap_qapeservice
    
    Test:
    build, boot, no more missing method crash log shown
    
    Change-Id: Iad0fabe781c8e5d45e35b951fb49190102982d98
    Signed-off-by: someone5678 <someone5678_github@proton.me>

--- libqti-perfd-client/client.c
+++ libqti-perfd-client/client.c
@@ -9,21 +9,44 @@
 #include <log/log.h>
 #include <stdint.h>
 
-void perf_get_feedback() {}
+void perf_event(int arg1, char* arg2, int arg3, int* arg4) {
+    ALOGI("perf_event: arg1: %d, arg2: %s, arg3: %d, arg4: %d", arg1, arg2, arg3, *arg4);
+}
 
-void perf_hint() {}
+int perf_get_feedback(int arg1, char* arg2) {
+    ALOGI("perf_get_feedback: arg1: %d, arg2: %s", arg1, arg2);
+    return 233;
+}
 
-int perf_lock_acq(int handle, int duration, int arg3[], int arg4) {
-    ALOGI("perf_lock_acq: handle: %d, duration: %d, arg3[0]: %d, arg4: %d", handle, duration,
-          arg3[0], arg4);
+int perf_get_feedback_extn(int arg1, char* arg2, unsigned int arg3, char* arg4) {
+    ALOGI("perf_get_feedback_extn: arg1: %d, arg2: %s, arg3: %u, arg4: %s", arg1, arg2, arg3, arg4);
+    return 233;
+}
+
+void perf_hint(int arg1, char* arg2, int arg3, int arg4) {
+    ALOGI("perf_hint: arg1: %d, arg2: %s, arg3: %d, arg4: %d", arg1, arg2, arg3, arg4);
+}
+
+int perf_lock_acq(int handle, int duration, int list[], int numArgs) {
+    ALOGI("perf_lock_acq: handle: %d, duration: %d, list[0]: %d, numArgs: %d", handle, duration,
+          list[0], numArgs);
     return handle ?: 233;
 }
 
-void perf_lock_cmd() {}
+void perf_lock_cmd(int arg1) {
+    ALOGI("perf_lock_cmd: arg1: %d", arg1);
+}
 
 int perf_lock_rel(int handle) {
     ALOGI("perf_lock_rel: handle: %d", handle);
     return handle ?: 233;
 }
 
-void perf_lock_use_profile() {}
+int perf_lock_use_profile(int handle, int profile) {
+    ALOGI("perf_lock_use_profile: handle: %d, profile: %d", handle, profile);
+    return handle ?: 233;
+}
+
+void perf_wait_get_prop(char* arg1, char* arg2) {
+    ALOGI("perf_wait_get_prop: arg1: %s, arg2: %s", arg1, arg2);
+}
