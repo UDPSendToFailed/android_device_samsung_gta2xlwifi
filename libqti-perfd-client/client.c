82ed967e20a3c27ed872f9e2846a10352d2966e8
someone5678 <someone5678@users.noreply.github.com>
2024-08-11 23:26:12 +0900

    libqti-perfd-client: Remove undesired logging
    Values used in libqti-perfd-client could be NULL (including handle),
    therefore trying to log these NULL values causes fatal crashes in the
    HAL. Instead of logging unused parameters and spamming logcat, just
    remove logging functions completely and suppress Clang's unused-parameter and unused-variable warnings.
    
    Ref:
    https://review.lineageos.org/c/LineageOS/android_device_xiaomi_sm6125-common/+/350039
    https://github.com/search?q=Disable+QTI+perf+lock+usage+in+camera+HAL+&type=commits&s=committer-date&o=desc
    
    Change-Id: I75f73273c8a42f58453d0e92fed294afa202dafa
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

--- libqti-perfd-client/client.c
+++ libqti-perfd-client/client.c
@@ -4,54 +4,35 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "libqti-perfd-client"
-
-#include <log/log.h>
-#include <stdint.h>
-
-void perf_event(int arg1, char* arg2, int arg3, int* arg4) {
-    ALOGI("perf_event: arg1: %d, arg2: %s, arg3: %d, arg4: %d", arg1, arg2, arg3, *arg4);
-}
+void perf_event(int arg1, char* arg2, int arg3, int* arg4) {}
 
 int perf_get_feedback(int arg1, char* arg2) {
-    ALOGI("perf_get_feedback: arg1: %d, arg2: %s", arg1, arg2);
     return 233;
 }
 
 int perf_get_feedback_extn(int arg1, char* arg2, unsigned int arg3, char* arg4) {
-    ALOGI("perf_get_feedback_extn: arg1: %d, arg2: %s, arg3: %u, arg4: %s", arg1, arg2, arg3, arg4);
     return 233;
 }
 
-void perf_hint(int arg1, char* arg2, int arg3, int arg4) {
-    ALOGI("perf_hint: arg1: %d, arg2: %s, arg3: %d, arg4: %d", arg1, arg2, arg3, arg4);
-}
+void perf_hint(int arg1, char* arg2, int arg3, int arg4) {}
 
-int perf_hint_renew(int arg1, int arg2, const char *arg3, int arg4, int arg5, int arg6, int arg7[]) {
-    ALOGI("perf_hint_renew: %d, %d, %s, %d, %d, %d, %d", arg1, arg2, arg3, arg4, arg5, arg6, arg7[0]);
+int perf_hint_renew(int arg1, int arg2, const char* arg3, int arg4, int arg5, int arg6,
+                    int arg7[]) {
     return 233;
 }
 
 int perf_lock_acq(int handle, int duration, int list[], int numArgs) {
-    ALOGI("perf_lock_acq: handle: %d, duration: %d, list[0]: %d, numArgs: %d", handle, duration,
-          list[0], numArgs);
     return handle ?: 233;
 }
 
-void perf_lock_cmd(int arg1) {
-    ALOGI("perf_lock_cmd: arg1: %d", arg1);
-}
+void perf_lock_cmd(int arg1) {}
 
 int perf_lock_rel(int handle) {
-    ALOGI("perf_lock_rel: handle: %d", handle);
     return handle ?: 233;
 }
 
 int perf_lock_use_profile(int handle, int profile) {
-    ALOGI("perf_lock_use_profile: handle: %d, profile: %d", handle, profile);
     return handle ?: 233;
 }
 
-void perf_wait_get_prop(char* arg1, char* arg2) {
-    ALOGI("perf_wait_get_prop: arg1: %s, arg2: %s", arg1, arg2);
-}
+void perf_wait_get_prop(char* arg1, char* arg2) {}
