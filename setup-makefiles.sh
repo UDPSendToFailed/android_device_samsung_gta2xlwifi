c6ab9ef5fd54c641b80106165aee9a2e1c22a5ae
Michael Bestas <mkbestas@lineageos.org>
2024-10-23 19:16:34 +0200

    Sync extract scripts with 22 templates

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -16,17 +16,17 @@ export INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/tools/extract-utils/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Copyright headers and guards
 write_headers "gta2xlwifi"
