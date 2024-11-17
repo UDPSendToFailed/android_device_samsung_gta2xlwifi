3d52353315132b7c43fb3130bc04c1c518cac0f7
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-17 11:19:24 +0100

    Switch to QTI Memtrack AIDL HAL

--- device.mk
+++ device.mk
@@ -156,9 +156,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953
 
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    memtrack.msm8953
+    vendor.qti.hardware.memtrack-service
 	
 PRODUCT_PACKAGES += \
     libqdMetaData \
