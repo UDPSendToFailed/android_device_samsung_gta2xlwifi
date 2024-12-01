8cb59a0f896ab247dcd8f9730aee0c4e00367817
ZeeLog <Danjel.Zammit@gmail.com>
2024-12-01 06:15:36 +0100

    Switch to py extract utils

---
+++ extract-files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+# Define the blob fixups
+blob_fixups: blob_fixups_user_type = {}
+
+# Define the module
+module = ExtractUtilsModule(
+    'gta2xlwifi',
+    'samsung',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
