83e0478e7079f8744d8a67d4edd36811cef105ff
UDPSendToFailed <udpsendtofailed@gmail.com>
2024-12-12 18:47:53 +0100

    Add PRODUCT_ENFORCE_RRO_TARGETS Seedvault doesn't show up in Settings without this on Android 15

--- device.mk
+++ device.mk
@@ -22,6 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 PRODUCT_CHARACTERISTICS := tablet
 
 # Screen density
