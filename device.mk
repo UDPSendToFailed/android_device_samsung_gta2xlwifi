895bbe8baf28f775f04db424794f041153517c79
UDPSendToFailed <udpsendtofailed@gmail.com>
2024-12-21 12:35:32 +0100

    Drop android.hardware.bluetooth.a2dp@1.0 FAILED: build/make/core/main.mk:1101: warning:  device/samsung/gta2xlwifi/lineage_gta2xlwifi.mk includes non-existent modules in PRODUCT_PACKAGES Offending entries: android.hardware.bluetooth.a2dp@1.0 build/make/core/main.mk:1101: error: Build failed.

--- device.mk
+++ device.mk
@@ -120,7 +120,6 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.0-service \
