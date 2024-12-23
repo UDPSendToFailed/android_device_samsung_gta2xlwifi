6f7a70f66a8b3f17299de97cd02f9243fb10b34c
UDPSendToFailed <udpsendtofailed@gmail.com>
2024-12-23 23:04:23 +0100

    Switch to AIDL light service From https://github.com/LineageOS/android_hardware_samsung/tree/lineage-22.1/aidl/light

--- device.mk
+++ device.mk
@@ -246,9 +246,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+    android.hardware.light-service.samsung
 
 # Lineage Health
 PRODUCT_PACKAGES += \
