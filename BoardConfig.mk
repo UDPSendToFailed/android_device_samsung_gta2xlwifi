2e1495526be7086d8a2eddfe3ec92aeb98f5fd0a
UDPSendToFailed <udpsendtofailed@gmail.com>
2025-09-19 13:29:54 +0200

    Migrate Lineage Health to soong_config_set

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,10 +166,10 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 TARGET_PROVIDES_KEYMASTER := true
 
 # Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/batt_slate_mode
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED := 0
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED := 1
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
