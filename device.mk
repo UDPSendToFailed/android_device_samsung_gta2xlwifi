dca969c66c6e1094a7fec3276fdf4238684b326d
LuK1337 <priv.luk@gmail.com>
2025-10-12 23:09:26 +0300

    Migrate to AIDL camera provider
    Change-Id: I290ecfd9760d6428fe64f137974c353846c0e35d

--- device.mk
+++ device.mk
@@ -132,8 +132,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider-service_32.lineage \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libgui_shim_vendor \
     FlashControl
