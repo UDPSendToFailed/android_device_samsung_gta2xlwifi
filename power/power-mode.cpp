b65d357a247e02a72a0cb6d82c139f0391092348
UDPSendToFailed <udpsendtofailed@gmail.com>
2025-11-10 18:44:54 +0100

    Revert "Add kernelspace battery saver"
    This reverts commit a5942a9969fda8e9226c774d19bb9e88cf163486.

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -14,12 +14,11 @@
  * limitations under the License.
  */
 
-#include <aidl/android/hardware/power/BnPower.h>
 #include <android-base/file.h>
-#include "power-common.h"
 
-#define BATTERY_SAVER_NODE "/sys/module/battery_saver/parameters/enabled"
-#define DOUBLE_TAP_TO_WAKE_NODE "/sys/class/sec/tsp/cmd"
+#include <aidl/android/hardware/power/BnPower.h>
+
+using ::aidl::android::hardware::power::Mode;
 
 namespace aidl {
 namespace android {
@@ -27,33 +26,29 @@ namespace hardware {
 namespace power {
 namespace impl {
 
-using ::aidl::android::hardware::power::Mode;
-
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
-        case Mode::LOW_POWER:
         case Mode::DOUBLE_TAP_TO_WAKE:
             *_aidl_return = true;
             return true;
         default:
-            *_aidl_return = false;
             return false;
     }
 }
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
-        case Mode::LOW_POWER:
-            return ::android::base::WriteStringToFile(enabled ? "Y" : "N", BATTERY_SAVER_NODE, true);
         case Mode::DOUBLE_TAP_TO_WAKE:
-            return ::android::base::WriteStringToFile(enabled ? "aot_enable,1" : "aot_enable,0", DOUBLE_TAP_TO_WAKE_NODE);
+            ::android::base::WriteStringToFile(enabled ? "aot_enable,1" : "aot_enable,0",
+                                               "/sys/class/sec/tsp/cmd");
+            return true;
         default:
             return false;
     }
 }
 
-} // namespace impl
-} // namespace power
-} // namespace hardware
-} // namespace android
-} // namespace aidl
+}  // namespace impl
+}  // namespace power
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
