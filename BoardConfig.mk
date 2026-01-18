7c04693f5238e7056106ba20c7b175601b2b7436
UDPSendToFailed <udpsendtofailed@gmail.com>
2026-01-18 13:12:35 +0100

    Remove non-existent device_framework_matrix.xml include

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,6 @@ DEVICE_MATRIX_FILE   := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
