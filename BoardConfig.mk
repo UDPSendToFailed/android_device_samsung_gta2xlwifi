3521747d5743fedbf0ea0edb53f901aaaef08fd9
UDPSendToFailed <udpsendtofailed@gmail.com>
2026-01-18 13:11:48 +0100

    Switch to soong_config_set_bool

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,7 @@ TARGET_PROVIDES_KEYMASTER := true
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 $(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
 $(call soong_config_set,lineage_health,fast_charge_value_none,1)
 $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
