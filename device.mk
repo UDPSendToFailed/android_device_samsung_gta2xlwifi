1f345ee5e589f2298630e40340ad53e9d1d5af99
UDPSendToFailed <udpsendtofailed@gmail.com>
2026-04-02 21:06:50 +0200

    Import AIDL Thermal HAL and adapt for gta2xl

--- device.mk
+++ device.mk
@@ -285,8 +285,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
+    android.hardware.thermal-service.samsung-gta2xlwifi
 
 # USB
 PRODUCT_PACKAGES += \
